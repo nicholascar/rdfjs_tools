c042a9993a23a704424230e09d136ac50147521e
ashleysommer <ashleysommer@gmail.com>
2015-09-10 14:33:27 +1000

    fixed rdfsrules to work with inferences in blank (anonymous) nodes.

--- ashdemo1/js/rdfsrules.js
+++ ashdemo1/js/rdfsrules.js
@@ -53,14 +53,26 @@ define(['rdfstore','async'], function (rdfstore,async) {
                 callback(err);
             });
         };
-
+        this._apply_rdf_types = function(store,subject,callback) {
+            var sparql = "" +
+                "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
+                "INSERT { "+subject+" rdf:type ?parent } " +     //child instanceof human
+                "WHERE { "+subject+" rdf:type ?class ." +     //tim instanceof child
+                        "?class rdfs:subClassOf ?parent }";// +      //child subclassof human
+            //"FILTER (?existing != ?parent) }";                //?existing != human
+            store.execute(sparql, function (err,results) {
+                if (!is_null_or_blank(err)) { callback(err); }
+                else { callback(null,results); }
+            });
+        };
         this._apply_rdfs_subclassof = function(store,subject,callback) {
             var sparql = "" +
                 "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
                 "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
-                "INSERT { <"+subject+"> rdf:type ?parent } " +       //child type human
-                "WHERE { <"+subject+"> rdfs:subClassOf ?parent }";// + //child subclassof human
-                 //       "<"+subject+"> rdf:type ?existing . " +      //child type ?existing
+                "INSERT { "+subject+" rdfs:Class ?parent } " +       //child class human
+                "WHERE { "+subject+" rdfs:subClassOf ?parent }";// + //child subclassof human
+                //        "<"+subject+"> rdf:type ?existing . " +      //child type ?existing
                 //"FILTER (?existing != ?parent) }";                //?existing != human
             store.execute(sparql, function (err,results) {
                if (!is_null_or_blank(err)) { callback(err); }
@@ -71,8 +83,8 @@ define(['rdfstore','async'], function (rdfstore,async) {
             var sparql = "" +
                 "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
                 "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
-                "INSERT { <"+subject+"> ?parent ?o } " +         // tim parent gwen
-                "WHERE { <"+subject+"> ?p ?o . " +               // tim mother gwen
+                "INSERT { "+subject+" ?parent ?o } " +         // tim parent gwen
+                "WHERE { "+subject+" ?p ?o . " +               // tim mother gwen
                          " ?p rdfs:subPropertyOf ?parent }";// +   //mother subpropertyof parent
                 //        " <"+subject+"> ?parent ?existing . " +  // tim mother ?exiting
                 //"FILTER (?existing != ?o) }";                    // ?existing != gwen
@@ -89,7 +101,7 @@ define(['rdfstore','async'], function (rdfstore,async) {
                 "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
                 //"SELECT * " +
                 "INSERT { ?o rdfs:Class ?rangeclass } " +      // gwen Class adultperson
-                "WHERE { <"+subject+"> ?p ?o . " +               // tim mother gwen
+                "WHERE { "+subject+" ?p ?o . " +               // tim mother gwen
                        " ?p rdfs:range ?rangeclass } ";// +         //mother range adultpersion
                        //" ?o rdfs:Class ?existing  }" //+           // gwen Class ?exiting
                 //"FILTER (?existing != ?rangeclass) }";           // ?existing != adultperson
@@ -104,8 +116,8 @@ define(['rdfstore','async'], function (rdfstore,async) {
             var sparql = "" +
                 "PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
                 "PREFIX rdfs:<http://www.w3.org/2000/01/rdf-schema#> " +
-                "INSERT { <"+subject+"> rdfs:Class ?domainc } " + // tim Class child
-                "WHERE { <"+subject+"> ?p ?o . " +                   // tim mother gwen
+                "INSERT { "+subject+" rdfs:Class ?domainc } " + // tim Class child
+                "WHERE { "+subject+" ?p ?o . " +                   // tim mother gwen
                        " ?p rdfs:domain ?domainc } ";// +               // mother domain child
                        //" <"+subject+"> rdfs:Class ?existing . " +    // tim Class ?exiting
                 //"FILTER (?existing != ?domainc) }";               // ?existing != child
@@ -131,6 +143,9 @@ define(['rdfstore','async'], function (rdfstore,async) {
                 },
                 function (c) {
                     that._apply_rdfs_range.call(that,store,subject,c);
+                },
+                function (c) {
+                    that._apply_rdf_types.call(that,store,subject,c);
                 }
             ],function (err,results){
                 if (!is_null_or_blank(err)) { callback(err); }
@@ -144,18 +159,21 @@ define(['rdfstore','async'], function (rdfstore,async) {
                 recursion = 0;
             }
             async.waterfall([function (callback) {
-                that._load_prereqs(store,function (err) {
-                    if (!is_null_or_blank(err)) {
-                        callback(err);
-                    }
-                    else {
-                        callback();
-                    }
-                });
-            },function (callback) {
-                if (recursion >= 4) {
-                    callback("recursion too deep!"); return;
+                if (recursion > 3) {
+                    callback("recursion too deep!");
+                } else if (recursion < 1) {
+                    that._load_prereqs(store, function (err) {
+                        if (!is_null_or_blank(err)) {
+                            callback(err);
+                        }
+                        else {
+                            callback();
+                        }
+                    });
+                } else {
+                    callback();
                 }
+            },function (callback) {
                 var select_statement = "SELECT ?s ?c WHERE { ?s rdf:type ?c }";
 
                 if (!is_null_or_blank(subject)) {
@@ -164,7 +182,7 @@ define(['rdfstore','async'], function (rdfstore,async) {
                         console.log("Already done full entailment for " + subject + "!");
                         callback(null,[]); return;
                     }
-                    select_statement = "SELECT ?c WHERE { <" + subject + "> rdf:type ?c }";
+                    select_statement = "SELECT ?c WHERE { " + subject + " rdf:type ?c }";
                 } else {
                     console.log("Entering apply_entailment for ALL with recursion of " + recursion);
                 }
@@ -181,60 +199,58 @@ define(['rdfstore','async'], function (rdfstore,async) {
                     }
                 });
             },function (results,callback) {
+                var apply_rules_to = function(subject2, cb) {
+                    that._apply_rdfs_rules.call(that, store, subject2, function () {
+                        that._applied_to.push(subject2);
+                        if (recursion >= 3) {
+                            cb();
+                        } else {
+                            that.apply_entailment(store, subject2, recursion + 1, function (err, results) {
+                                cb(err);
+                            });
+                        }
+                    });
+                };
                 async.eachSeries(results,function (item,callback2) {
                     //Skip it if there was no object
                     if (is_null_or_blank(item) || is_null_or_blank(item.c)){
                         async.setImmediate(function () { callback2(); });
                         return;
                     }
-                    if (!is_null_or_blank(item.s) && !is_null_or_blank(item.s.value)){
+                    if (!is_null_or_blank(item.s) && !is_null_or_blank(item.s.token) && !is_null_or_blank(item.s.value)){
                         async.setImmediate(function() {
-                            console.log("Applying rules to [s] item "+item.s.value+" recursion level "+recursion);
-                            that._apply_rdfs_rules.call(that, store, item.s.value, function () {
-                                that._applied_to.push(item.s.value);
-                                if (recursion >= 3) {
-                                    callback2(); return;
-                                }
-                                that.apply_entailment(store, item.s.value, recursion + 1, function (err, results) {
-                                    callback2(err);
-                                });
-                            });
+                            var s = item.s.value;
+                            if (item.s.token == "uri")
+                            {
+                                s = "<"+item.s.value+">";
+                            }
+                            console.log("Applying rules to [s] item "+s+" recursion level "+recursion);
+                            apply_rules_to(s, callback2);
                         });
                         return;
                     }
-                    var obj = item.c;
-                    if (that._has_applied_to(obj.value)) {
-                        console.log("Already applied rules to "+obj.value+" skipping whole class.");
+                    var s = item.c.value;
+                    if (item.c.token == "uri") {
+                        s = "<"+item.c.value+">";
+                    }
+                    if (that._has_applied_to(s)) {
+                        console.log("Already applied rules to "+s+" skipping whole object.");
                         async.setImmediate(function () { callback2(); });
                         return;
                     }
                     async.setImmediate(function () {
-                        console.log("Applying rules to [c] item "+obj.value+" recursion level "+recursion);
-                        if (obj.token && obj.token == "uri"){
-                            load_remote_if_not_present(store, obj.value, function (err,result) {
+                        console.log("Applying rules to [c] item "+s+" recursion level "+recursion);
+                        if (item.c.token && item.c.token == "uri"){
+                            load_remote_if_not_present(store, item.c.value, function (err,result) {
                                 if (!is_null_or_blank(err)) {
                                     callback2(err);
-                                    return;
+                                } else {
+                                    apply_rules_to(s, callback2);
                                 }
-                                that._apply_rdfs_rules.call(that, store, obj.value, function () {
-                                    that._applied_to.push(obj.value);
-                                    if (recursion >= 3) {
-                                        callback2(); return;
-                                    }
-                                    return that.apply_entailment(store, obj.value, recursion + 1, function (err, results) {
-                                        callback2(err);
-                                    });
-                                });
+
                             });
                         } else {
-                            that._apply_rdfs_rules.call(that, store, obj.value, function () {
-                                if (recursion >= 3) {
-                                    callback2(); return;
-                                }
-                                return that.apply_entailment(store, obj.value, recursion + 1, function (err, results) {
-                                    callback2(err);
-                                });
-                            });
+                            apply_rules_to(s,callback2);
                         }
                     });
                 },function(err,results){
