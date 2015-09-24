e3dca1ec5b94923aa27c3f6f85718c4be1a14f09
ashleysommer <ashleysommer@gmail.com>
2015-09-24 22:05:21 +1000

    Added template renderer based on high speed rendering engine.

---
+++ ashdemo1/js/tmpl.js
@@ -0,0 +1,40 @@
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+var cache = {};
+
+var tmpl = function (str, data, fn_only){
+    // Figure out if we're getting a template, or if we need to
+    // load the template - and be sure to cache the result.
+    var fn = !/\W/.test(str) ?
+        // str is a word, so it is a template id, or a cache id.
+        //Load it from the cache or construct it form the id
+        cache[str] = cache[str] ||
+            tmpl(document.getElementById(str).innerHTML,data,true) :
+
+        // else, the arg is a HTML string, so use it is the template contents.
+        // Generate a reusable function that will serve as a template
+        // generator (and which will be cached).
+        new Function("obj",
+            "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+            // Introduce the data as local variables using with(){}
+            "with(obj){p.push('" +
+
+            // Convert the template into pure JavaScript
+            str.replace(/[\r\t\n]/g, " ")
+               .split("<%").join("\t")
+               .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+               .replace(/\t=(.*?)%>/g, "',$1,'")
+               .split("\t").join("');")
+               .split("%>").join("p.push('")
+               .split("\r").join("\\'")
+            + "');}return p.join('');");
+
+    // Provide some basic currying to the user
+    return fn_only? fn : ( data ? fn( data ) : fn({}) );
+};
+define([], function () {
+    var templ = function () {};
+    templ.prototype.tmpl = tmpl;
+    return new templ();
+});
